c3e72f27f707ea9c6e09679cf8909496d44ac59f
Christos Ferderigos <ferderigosc@gmail.com>
2020-01-21 23:36:01 -0500

    push

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -8,6 +8,21 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)). \
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+
 engine = create_engine("sqlite:///../Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
 
@@ -62,6 +77,13 @@ def tobs():
         tobs_dict.update({row.date: row.tobs})
         tobs_list.append(tobs_dict)
     return jsonify(tobs_list)
+
+@app.route('/api/v1.0/datesearch/<startDate>/<endDate>')
+def start(startDate,endDate):
+    range_results = calc_temps(startDate,endDate)
+    return jsonify(range_results)
+
+
 
 
 if __name__ == "__main__":
